465ae49790ec6fc43d01dc6ed2247993eb38220f
KristoferVickers <KRISTOFERVICKERS2018@GMAIL.COM>
2023-11-20 14:40:05 -0600

    Added Files & Folders,Added my Code,Updated README

---
+++ Develop/Assets/index.js
@@ -0,0 +1,114 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require('./utils/generateMarkdown.js');
+const questions = [
+
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter the title of your project',
+    },
+    {
+      type: 'input',
+      name: 'description', 
+      message: 'Breifly describe your application',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Explain how to install the application'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide examples how the application is used'        
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Which license will you use for your project?',
+        choices: ['mit', 'GPLv2', 'Apache', 'no license']
+    },
+    {
+        type: 'confirm',
+        name: 'confirmContributers',
+        message: 'Will there be contributers to this application?',
+        default: true
+    },
+    {
+        type: 'input',
+        name: 'contribute',
+        message: 'Please provide guidelines for contributing.',
+        when: ({ confirmContributers }) => {
+            if (confirmContributers) {
+                return true;
+            } else {
+                message: 'No contrinutions';
+            }
+        },
+        validate: contributerInput => {
+            if (contributerInput) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Provide instructions and examples of how the code is tested:',
+    },
+
+    {
+        type: 'input',
+        name: 'githubUsername',
+        message: 'What is your GitHub Username? (Required)',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address? (Required)',
+    },
+
+
+];
+const writeFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./newREADME/generated-README.md', fileContent, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            resolve({
+                ok: true,
+                message: 'File created!'
+            });
+        });
+    });
+};
+
+const init = () => {
+
+    return inquirer.prompt(questions)
+    .then(readmeData => {
+        return readmeData;
+    })
+}
+init()
+.then(readmeData => {
+    console.log(readmeData);
+    return generateMarkdown(readmeData);
+})
+.then(pageMD => {
+    return writeFile(pageMD);
+})
+.then(writeFileResponse => {
+    console.log(writeFileResponse.message);
+})
+.catch(err => {
+    console.log(err);
+})
+
+init();
--- README.md
+++ README.md
@@ -2,10 +2,43 @@
 
 ## URL :
 
+https://github.com/KristoferVickers/readme-generator
+
 ## Discription :
+
+This is a project made out of using nodejs. Using the intergrated terminal on the index.js file. Using node index.js it should prompt you to fill out a couple questions about what the generator is going to be used for. Then once done filling out the information it wants it will generate and pass. 
 
 ## Acceptance Criteria :
 
-## Screen Shot :
+GIVEN a command-line application that accepts user input
 
-## Sources :
+WHEN I am prompted for information about my application repository
+
+THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+
+WHEN I enter my project title
+
+THEN this is displayed as the title of the README
+
+WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+
+THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+
+WHEN I choose a license for my application from a list of options
+
+THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+
+WHEN I enter my GitHub username
+
+THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+
+WHEN I enter my email address
+
+THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+
+WHEN I click on the links in the Table of Contents
+
+THEN I am taken to the corresponding section of the README
+
+
+## Screen Shot :
