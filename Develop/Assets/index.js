ea623ae155827cb142c4f7735d193147698ff090
KristoferVickers <KRISTOFERVICKERS2018@GMAIL.COM>
2023-11-20 15:16:25 -0600

    Intergrated termail work being added.

--- Develop/Assets/index.js
+++ Develop/Assets/index.js
@@ -1,108 +1,98 @@
-const fs = require("fs");
-const inquirer = require("inquirer");
-const generateMarkdown = require('./generateMarkdown');
-const questions = [
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-    {
-        type: 'input',
-        name: 'title',
-        message: 'Enter the title of your project',
-    },
-    {
-      type: 'input',
-      name: 'description', 
-      message: 'Breifly describe your application',
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'Explain how to install the application'
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'How the application will be used'        
-    },
-    {
-        type: 'list',
-        name: 'license',
-        message: 'Which license will you use for your project?',
-        choices: ['mit', 'GPLv2', 'Apache', 'no license']
-    },
-    {
-        type: 'confirm',
-        name: 'confirmContributers',
-        message: 'Will there be contributers to this application?',
-        default: true
-    },
-    {
-        type: 'input',
-        name: 'contribute',
-        message: 'Please provide guidelines for contributing.',
-        when: ({ confirmContributers }) => {
-            if (confirmContributers) {
-                return true;
-            } else {
-                message: 'No contrinutions';
-            }
-        },
-        validate: contributerInput => {
-            if (contributerInput) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'test',
-        message: 'Provide instructions and examples of how the code is tested:',
-    },
+function generateREADME(answers) {
+  return `
+# ${answers.title}
 
-    {
-        type: 'input',
-        name: 'githubUsername',
-        message: 'What is your GitHub Username? (Required)',
-    }, 
-    
-];
-const writeFile = fileContent => {
-    return new Promise((resolve, reject) => {
-        fs.writeFile('./newREADME/generated-README.md', fileContent, err => {
-            if (err) {
-                reject(err);
-                return;
-            }
+## Description
+${answers.description}
 
-            resolve({
-                ok: true,
-                message: 'File created!'
-            });
-        });
-    });
-};
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
 
-const init = () => {
+## Installation
+${answers.installation}
 
-    return inquirer.prompt(questions)
-    .then(readmeData => {
-        return readmeData;
-    })
+## Usage
+${answers.usage}
+
+## License
+![License](https://img.shields.io/badge/license-${answers.license}-brightgreen)
+
+This project is licensed under the ${answers.license} license.
+
+## Contributing
+${answers.contributing}
+
+## Tests
+${answers.tests}
+
+## Questions
+For questions about this project, contact [${answers.username}](https://github.com/${answers.username}) at ${answers.email}.
+`;
 }
-init()
-.then(readmeData => {
-    console.log(readmeData);
-    return generateMarkdown(readmeData);
-})
-.then(pageMD => {
-    return writeFile(pageMD);
-})
-.then(writeFileResponse => {
-    console.log(writeFileResponse.message);
-})
-.catch(err => {
-    console.log(err);
-})
 
-init();
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'Enter the title of your project:',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a short description of your project:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Provide installation instructions:',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Provide usage information:',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your application:',
+    choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3-Clause', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Provide contribution guidelines:',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Provide test instructions:',
+  },
+  {
+    type: 'input',
+    name: 'username',
+    message: 'Enter your GitHub username:',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address:',
+  },
+];
+
+
+inquirer.prompt(questions).then((answers) => {
+
+  const readmeContent = generateREADME(answers);
+
+  fs.writeFileSync('README.md', readmeContent);
+
+  console.log('README.md successfully generated!');
+});
